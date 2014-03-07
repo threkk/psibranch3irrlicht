ba117bb6431716d37ea54e588ae6505554b6e03f
Alberto martinez de murga ramirez <alberto.martinez.de.murga.ramirez@hva.nl>
2014-03-07 14:29:52 +0100

    Added the header to the include folder.

---
+++ source/Irrlicht/ISceaneSeamlessLoader.h
@@ -0,0 +1,37 @@
+#ifndef __I_SEAMLESS_SCENE_LOADER_H_INCLUDED_
+#define __I_SEAMLESS_SCENE_LOADER_H_INCLUDED_
+
+#include "irrlicht.h"
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+using namespace irr;
+using namespace scene;
+
+namespace irr {
+namespace scene 
+{
+	class ISceneSeamlessLoader 
+	{
+	private: 
+		typedef struct {
+			int position;
+			ISceneManager* smngr;
+			io::path uri;
+		} NScene;
+
+		ISceneManager* actual;
+		std::vector<NScene>* scenes;
+		
+	public:
+		ISceneSeamlessLoader(ISceneManager* smgr);
+		~ISceneSeamlessLoader();
+
+		virtual int preloadScene(irr::io::path uriToNewScene);
+		virtual bool dropPreloadedScene(int sceneIndex);
+		virtual void switchScene(int sceneIndex);
+	};
+}
+}
+#endif
