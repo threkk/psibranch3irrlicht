845304f56901f472cb90e0b6fcbe3e1e6186d4ae
RM van Diepen <rob.van.diepen@hva.nl>
2014-04-11 13:23:37 +0200

    corrections on file locations

--- source/Irrlicht/AI/StateFollowTarget.cpp
+++ source/Irrlicht/AI/StateFollowTarget.cpp
@@ -12,9 +12,6 @@ StateFollowTarget::StateFollowTarget(Detectable* stateOwner, Detectable* target,
 
 	this->callbackFunction = callbackFunction;
 	this->pathUtil = pathUtil;
-
-	hasSeen = false;
-	hasLastPointSeen = false;
 }
 
 
@@ -24,74 +21,35 @@ StateFollowTarget::~StateFollowTarget(void)
 
 bool StateFollowTarget::executeable(void)
 {
-	// The state will execute as soon as the player is seen
-	if(owner->isObjectInfront(target, sceneMgr))
-	{
-		hasSeen = true;
-		lastPointSeen = this->target->getPosition();
-		return true;
-	}
-	//If the player is not seen check if there is a lastPointSeen
-	else if(hasSeen || hasLastPointSeen)
-	{
-		hasSeen = false;
-		hasLastPointSeen = true;
-		return true;
-	}
-	//Otherwise return false
-	else
-	{
-		hasSeen = false;
-		hasLastPointSeen = false;
-		return false;
-	}
+	return true;
 }
 
-void StateChaseTarget::enter()
+void StateFollowTarget::enter()
 {
 }
 
-void StateChaseTarget::exit()
+void StateFollowTarget::exit()
 {
 }
 
-void StateChaseTarget::action()
+void StateFollowTarget::action()
 {
-	irr::core::vector3df toPos;
-	if(hasSeen)
-	{
-		toPos = this->target->getGroundPosition();
-	}
-	else if(hasLastPointSeen)
-	{
-		toPos = lastPointSeen;
-	}
-	else
-	{
-		return;
-	}
 	// Get position of object by the pathfinding utility
-	std::vector<irr::core::vector3df> path = pathUtil->returnPath(&this->owner->getPosition(), &toPos);
+	std::vector<irr::core::vector3df> path = pathUtil->returnPath(&this->owner->getPosition(), &this->target->getGroundPosition());
 	
 	// If there is a path to the object walk to it
 	if ( !path.empty() && path.size() > 1 )
 	{
 		// Don't walk right into the target
-		if (this->owner->getPosition().getDistanceFrom(irr::core::vector3df(toPos.X, toPos.Y, toPos.Z)) > 100)
+		if (this->owner->getPosition().getDistanceFrom(this->target->getGroundPosition()) > 100)
 		{
 			// Call callback method
 			callbackFunction(&path.at(1));
 		}
-		else
-		{
-			hasLastPointSeen = false;
-			hasSeen = false;
-			stateMachine->returnToPreviousState();
-		}
 	}
 }
 
-void StateChaseTarget::noActionFallback()
+void StateFollowTarget::noActionFallback()
 {
 	stateMachine->returnToPreviousState();
 }
