df3d187a868cfbed06936c2aea4ac86e97db65e9
JGH Rommers <joey.rommers@hva.nl>
2014-04-30 00:56:25 +0200

    Changed modelname

--- include/ParticleParser.h
+++ include/ParticleParser.h
@@ -30,22 +30,6 @@ public:
 
 private:
 
-	float dirX, dirY, dirZ; // floats to store the direction
-	float minX, minY, minZ, maxX, maxY, maxZ; // floats to store the AABBOX
-	float posX, posY, posZ; // Position
-	float centerX, centerY, centerZ; // Center
-	float normalX, normalY, normalZ; // Normal
-
-
-	int emitterTypeINT;// int to store the emitter type
-	EmitterTypes emitterType;
-	s32 maxAngleAdegrees, mbNumber; // AngleDegrees
-	u32 lifeTimeMin, lifeTimeMax, minPPS, maxPPS;// LifeTime
-	s32 particlesPerSecond;// Particles Per Second
-	video::SColor minStartColor, maxStartColor;
-	core::dimension2df minStartSize, maxStartSize;
-	core::stringc pathNameTexture;
-
 };
 
 #endif
--- source/Irrlicht/ParticleParser.cpp
+++ source/Irrlicht/ParticleParser.cpp
@@ -9,7 +9,7 @@ ParticleParser::ParticleParser()
 	// Creates a XML reader and set path to load the XML file from
 	io::IrrXMLReader* xmlReader = io::createIrrXMLReader("../../Media/test.xml");
 
-	ParticleModel model;
+	ParticleModel modelXML;
 
 	while (xmlReader && xmlReader->read())
 	{
@@ -20,112 +20,114 @@ ParticleParser::ParticleParser()
 			{
 				if (!strcmp("Direction", xmlReader->getNodeName()))
 				{
-					model.setDirection(core::vector3df(xmlReader->getAttributeValueAsFloat("X"),
+					modelXML.setDirection(core::vector3df(xmlReader->getAttributeValueAsFloat("X"),
 						xmlReader->getAttributeValueAsFloat("Y"),
 						xmlReader->getAttributeValueAsFloat("Z")));
 
-					std::cout << "Direction: " << model.getDirection().X << std::endl;
-					std::cout << "Direction: " << model.getDirection().Y << std::endl;
-					std::cout << "Direction: " << model.getDirection().Z << std::endl;
+					std::cout << "Direction: " << modelXML.getDirection().X << std::endl;
+					std::cout << "Direction: " << modelXML.getDirection().Y << std::endl;
+					std::cout << "Direction: " << modelXML.getDirection().Z << std::endl;
 					
 				}
 				else if (!strcmp("Emitter", xmlReader->getNodeName()))
 				{
-					model.setEmitterType(ParticleModel::EmitterTypes(xmlReader->getAttributeValueAsInt("Type")));
+					modelXML.setEmitterType(ParticleModel::EmitterTypes(xmlReader->getAttributeValueAsInt("Type")));
 
-					std::cout << "Emitter type: " << model.getEmitterType() << std::endl;
+					std::cout << "Emitter type: " << modelXML.getEmitterType() << std::endl;
 					
 				}
 				else if (!strcmp("Aabbox", xmlReader->getNodeName()))
 				{
-					model.setAabbox(core::aabbox3df(xmlReader->getAttributeValueAsFloat("MinX"), xmlReader->getAttributeValueAsFloat("MinY"),
+					modelXML.setAabbox(core::aabbox3df(xmlReader->getAttributeValueAsFloat("MinX"), xmlReader->getAttributeValueAsFloat("MinY"),
 					xmlReader->getAttributeValueAsFloat("MinZ"), xmlReader->getAttributeValueAsFloat("MaxX"),
 					xmlReader->getAttributeValueAsFloat("MaxY"), xmlReader->getAttributeValueAsFloat("MaxZ")));
 
-					std::cout << "Aabbox: minX" << model.getAabbox().MinEdge.X << " minY : " << model.getAabbox().MinEdge.Y << " minZ : " << model.getAabbox().MinEdge.Z << std::endl;
-					std::cout << "Aabbox: maxX" << model.getAabbox().MaxEdge.X << " maxY : " << model.getAabbox().MaxEdge.Y << " maxZ : " << model.getAabbox().MaxEdge.Z << std::endl;
+					std::cout << "Aabbox: minX : " << modelXML.getAabbox().MinEdge.X << " minY : " << modelXML.getAabbox().MinEdge.Y << " minZ : " << modelXML.getAabbox().MinEdge.Z << std::endl;
+					std::cout << "Aabbox: maxX : " << modelXML.getAabbox().MaxEdge.X << " maxY : " << modelXML.getAabbox().MaxEdge.Y << " maxZ : " << modelXML.getAabbox().MaxEdge.Z << std::endl;
 
 				}
 				else if (!strcmp("Position", xmlReader->getNodeName()))
 				{
-					model.setPosition(core::vector3df(xmlReader->getAttributeValueAsFloat("X"),
-					xmlReader->getAttributeValueAsFloat("Y"), posZ = xmlReader->getAttributeValueAsFloat("Z")));
+					modelXML.setPosition(core::vector3df(xmlReader->getAttributeValueAsFloat("X"),
+					xmlReader->getAttributeValueAsFloat("Y"), xmlReader->getAttributeValueAsFloat("Z")));
 
-					std::cout << "Position: X: " << model.getPosition().X << " Position Y: " << model.getPosition().Y << " Position Z: " << model.getPosition().Z << std::endl;
+					std::cout << "Position: X: " << modelXML.getPosition().X << " Position Y: " << modelXML.getPosition().Y << " Position Z: " << modelXML.getPosition().Z << std::endl;
 				}
 				else if (!strcmp("Center", xmlReader->getNodeName()))
 				{
-					model.setCenter(core::vector3df(xmlReader->getAttributeValueAsFloat("X"), xmlReader->getAttributeValueAsFloat("Y"),
+					modelXML.setCenter(core::vector3df(xmlReader->getAttributeValueAsFloat("X"), xmlReader->getAttributeValueAsFloat("Y"),
 						xmlReader->getAttributeValueAsFloat("Z")));
 
-					std::cout << "Center X: " << model.getCenter().X << " Center Y: " <<  model.getCenter().Y << " Center Z: " << model.getCenter().Z << std::endl;
+					std::cout << "Center X: " << modelXML.getCenter().X << " Center Y: " <<  modelXML.getCenter().Y << " Center Z: " << modelXML.getCenter().Z << std::endl;
 				}
 				else if (!strcmp("Normal", xmlReader->getNodeName()))
 				{
-					model.setNormal(core::vector3df(xmlReader->getAttributeValueAsFloat("X"), xmlReader->getAttributeValueAsFloat("Y"),
+					modelXML.setNormal(core::vector3df(xmlReader->getAttributeValueAsFloat("X"), xmlReader->getAttributeValueAsFloat("Y"),
 						xmlReader->getAttributeValueAsFloat("Z")));
 
-					std::cout << "Normal X: " << model.getNormal().X << " Normal Y: " << model.getNormal().Y << " Normal Z: " << model.getNormal().Z << std::endl;
+					std::cout << "Normal X: " << modelXML.getNormal().X << " Normal Y: " << modelXML.getNormal().Y << " Normal Z: " << modelXML.getNormal().Z << std::endl;
 				}
 				else if (!strcmp("AngleDegrees", xmlReader->getNodeName()))
 				{
-					model.setMaxAngleDegrees(s32(xmlReader->getAttributeValueAsInt("Max")));
+					modelXML.setMaxAngleDegrees(s32(xmlReader->getAttributeValueAsInt("Max")));
 
-					std::cout << "Angle: " << model.getMaxAngleDegrees() << std::endl;
+					std::cout << "Angle: " << modelXML.getMaxAngleDegrees() << std::endl;
 				}
 				else if (!strcmp("LifeTime", xmlReader->getNodeName()))
 				{
-					model.setLifeTimeMax(u32(xmlReader->getAttributeValueAsInt("Max")));
-					model.setLifeTimeMin(u32(xmlReader->getAttributeValueAsInt("Min")));
+					modelXML.setLifeTimeMax(u32(xmlReader->getAttributeValueAsInt("Max")));
+					modelXML.setLifeTimeMin(u32(xmlReader->getAttributeValueAsInt("Min")));
 
-					std::cout << "Lifetime Min: " << model.getLifeTimeMin() << " Max: " << model.getLifeTimeMax() << std::endl;
+					std::cout << "Lifetime Min: " << modelXML.getLifeTimeMin() << " Max: " << modelXML.getLifeTimeMax() << std::endl;
 				}
 				else if (!strcmp("PPS", xmlReader->getNodeName()))
 				{
-					model.setMaxPPS(u32(xmlReader->getAttributeValueAsInt("Max")));
-					model.setMinPPS(u32(xmlReader->getAttributeValueAsInt("Min")));
+					modelXML.setMaxPPS(u32(xmlReader->getAttributeValueAsInt("Max")));
+					modelXML.setMinPPS(u32(xmlReader->getAttributeValueAsInt("Min")));
 
-					std::cout << "PPS Min: " << model.getMinPPS() << " Max: " << model.getMaxPPS() << std::endl;
+					std::cout << "PPS Min: " << modelXML.getMinPPS() << " Max: " << modelXML.getMaxPPS() << std::endl;
 				}
 				else if (!strcmp("MaxStartColor", xmlReader->getNodeName()))
 				{
-					model.setMaxColor(video::SColor(xmlReader->getAttributeValueAsInt("Alpha"), xmlReader->getAttributeValueAsInt("Red"),
+					modelXML.setMaxColor(video::SColor(xmlReader->getAttributeValueAsInt("Alpha"), xmlReader->getAttributeValueAsInt("Red"),
 						xmlReader->getAttributeValueAsInt("Blue"), xmlReader->getAttributeValueAsInt("Green")));
 
 					
 				}
 				else if (!strcmp("MinStartColor", xmlReader->getNodeName()))
 				{
-					model.setMinColor(video::SColor(xmlReader->getAttributeValueAsInt("Alpha"), xmlReader->getAttributeValueAsInt("Red"),
+					modelXML.setMinColor(video::SColor(xmlReader->getAttributeValueAsInt("Alpha"), xmlReader->getAttributeValueAsInt("Red"),
 						xmlReader->getAttributeValueAsInt("Blue"), xmlReader->getAttributeValueAsInt("Green")));
 				}
 				else if (!strcmp("MaxStartSize", xmlReader->getNodeName()))
 				{
-					model.setMaxStartSize(core::dimension2df(xmlReader->getAttributeValueAsFloat("Width"), xmlReader->getAttributeValueAsFloat("Height")));
+					modelXML.setMaxStartSize(core::dimension2df(xmlReader->getAttributeValueAsFloat("Width"), xmlReader->getAttributeValueAsFloat("Height")));
 				}
 				else if (!strcmp("MinStartSize", xmlReader->getNodeName()))
 				{
-					model.setMinStartSize(core::dimension2df(xmlReader->getAttributeValueAsFloat("Width"), xmlReader->getAttributeValueAsFloat("Height")));
+					modelXML.setMinStartSize(core::dimension2df(xmlReader->getAttributeValueAsFloat("Width"), xmlReader->getAttributeValueAsFloat("Height")));
 				}
 				else if (!strcmp("Texture", xmlReader->getNodeName()))
 				{
-					model.setPathNameTexture(core::stringc(xmlReader->getAttributeValue("PathName")));
+					modelXML.setPathNameTexture(core::stringc(xmlReader->getAttributeValue("PathName")));
 				}
 				else if (!strcmp("Rest", xmlReader->getNodeName()))
 				{
-					model.setOutLineOnly(s32(xmlReader->getAttributeValueAsInt("OutLineOnly")));
-					model.setEveryMeshVertex(s32(xmlReader->getAttributeValueAsInt("EveryMeshVertex")));
-					model.setUseNormalDirection(s32(xmlReader->getAttributeValueAsInt("UseNormalDirection")));
-					model.setRingThickness(f32(xmlReader->getAttributeValueAsFloat("RingThickness")));
-					model.setLengthCylinder(f32(xmlReader->getAttributeValueAsFloat("LengthCylinder")));
-					model.setRadius(f32(xmlReader->getAttributeValueAsFloat("Radius")));
-					model.setNormalDirectionModifier(f32(xmlReader->getAttributeValueAsFloat("NormalDirectionModifier")));
-					model.setMbNumber(s32(xmlReader->getAttributeValueAsInt("MbNumber")));
+					modelXML.setOutLineOnly(s32(xmlReader->getAttributeValueAsInt("OutLineOnly")));
+					modelXML.setEveryMeshVertex(s32(xmlReader->getAttributeValueAsInt("EveryMeshVertex")));
+					modelXML.setUseNormalDirection(s32(xmlReader->getAttributeValueAsInt("UseNormalDirection")));
+					modelXML.setRingThickness(f32(xmlReader->getAttributeValueAsFloat("RingThickness")));
+					modelXML.setLengthCylinder(f32(xmlReader->getAttributeValueAsFloat("LengthCylinder")));
+					modelXML.setRadius(f32(xmlReader->getAttributeValueAsFloat("Radius")));
+					modelXML.setNormalDirectionModifier(f32(xmlReader->getAttributeValueAsFloat("NormalDirectionModifier")));
+					modelXML.setMbNumber(s32(xmlReader->getAttributeValueAsInt("MbNumber")));
 				}
 			}
 			break;
 		}
 	}
+
+
 	
 	// Delete the xml parser after usage
 	delete xmlReader;
